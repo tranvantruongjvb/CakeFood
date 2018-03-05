584bada045758148f83d7d751d11148121c86a1f
tranvantruongjvb <34053656+tranvantruongjvb@users.noreply.github.com>
2018-03-05 17:24:13 +0700

    add function editproduct và deleteproduct

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -34,23 +34,56 @@ class ProductsController  extends AppController{
             $data = $this->request->data();
             $this->Products->patchEntity($product,$this->request->data);
 
-                $name=$data['image'];
-                  $dir = WWW_ROOT . 'upload/' . $name['name'] ;
-                $a = move_uploaded_file($data['image']['tmp_name'], $dir);
+            $name=$data['image'];
+            $dir = WWW_ROOT .'img\uploads\ '. $name['name'] ;
+            $a = move_uploaded_file($data['image']['tmp_name'], $dir);
+            $product['image'] = 'http://localhost/cakecosy/webroot/img/uploads/'.$name['name'];
+            echo "$dir";
+            
         }
+
 
         // //now do the save
         if ($this->Products->save($product)) 
         {       
-            $this->Flash->success(__('The employee has been saved.'));
-
+            $this->Flash->success(__('The product has been saved.'));
+            
             return $this->redirect(URL_INDEX);
         } else {
-            $this->Flash->error(__('The employee could not be saved. Please, try again.'));
+            $this->Flash->error(__('The product could not be saved. Please, try again.'));
         }   
         $this->set('product',$product);
-}
+    }
        
- 
+    public function editproduct($id)
+    {   
+        $product = $this->Products->get($id);
+        if ($this->request->is(['post', 'put'])) {
+            $name = $this->request->data['name'];
+            $this->Products->patchEntity($product, $this->request->data);
+            if ($this->Products->save($product)) {
+               // $this->Email->sendUserEmail($this->request->data['email'],'Update profile',$name,'update');
+                $this->Flash->success(__('Your information data has been updated.'));
+                return $this->redirect(URL_INDEX);
+            }
+            $this->Flash->error(__('Unable to update your profile.'));
+        }
+    
+        $this->set('product', $product);      
+        
+    }
+
+    public function delete($id)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+    
+        $user = $this->Users->get($id);
+        if ($this->Users->delete($user)) {
+            $this->Flash->success(__('The user with id: {0} has been deleted.', h($id)));
+            return $this->redirect(URL_INDEX);
+        }       
+        
+    }
+   
 }
 ?>
