a9a68138bc8a15278ce2cee45e662d3bacb2384d
tranvantruongjvb <34053656+tranvantruongjvb@users.noreply.github.com>
2018-04-06 14:38:14 +0700

    Update ProductsController.php

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -27,18 +27,38 @@ class ProductsController  extends AppController{
 		$this->Auth->allow(['logout']);
 
     }
+    public function getnews()
+    {
+          $news = TableRegistry::get('get_email');
+        if($this->request->is('post')) {
+
+            $data = $this->request->data();           
+            print_r($data['email']);die;
+
+           
+                 $email = $data['your_email'];
+                 $array  = array('email'=>$email);
+                 $get = $news->newEntity();
+                 $get->email = $email;
+                 $news->save($get);
+                 $this->Email->sendUserEmail($email,'CakeFoody',$array,'getnews');
+                 $this->Flash->success(__('Cảm ơn bạn đăng ký'));
+                 $this->redirect($this->referer());
+            
+           
+        
+        }
+
+    }
 
     public function index()
 	{
 
         $promotion = $this->Products->find("all")
-        ->where( ['products.promotion_price >=' => 1]);
+        ->where( ['products.promotion_price >=' => 1])
+        ->order(['products.id' => 'DESC']);
         $this->paginate= array(
-            'limit' => 8,
-            'order' => [
-            'products.id' =>  'asc',
-            ]
-        );
+            'limit' => 8);
         $promotion_price = $this->paginate($promotion);
         $this->set('promotion_price',$promotion_price);
 
@@ -106,6 +126,7 @@ class ProductsController  extends AppController{
         $s= $this->request->data;
         $id = $s['id'];
         $b = $s['sl'];
+
         $session = $this->request->session();
        if ($session->check('cart.'.$id)) {
             $items = $session->read('cart.'.$id);
@@ -140,79 +161,31 @@ class ProductsController  extends AppController{
                 $items = array(
                 $totalPrice =  $value['quantity'] * $value['price']);
             }
-            $session->write('order', $totalPrice, array('timeout' => 1));
+            $session->write('order', $totalPrice);
              $s=$session->read('order');
 
         }
        
     }
 
-    public function postCheckout(){
-        
-        $session = $this->request->session();
-        // khai báo database để lấy dữ liệu table
+    public function customerbill($id)
+    {
+        $this->readtypeproduct();
         $cus = TableRegistry::get('customer');
-        $getbill = TableRegistry::get('bills');
-        $get_bill_detail = TableRegistry::get('bill_detail');
+        $getid = $cus->get($id)->id;
 
-        // read session
-        $cart = $session->read('cart');
-        $totalPrice = $session->read('payment.total');
-        $req= $this->request->query;
+        $getcus= $cus->find('all')
+        ->where(['customer.id_user = ' => $getid ])
+        ->toArray();
+        $this->set('billcustomer',$getcus);
+    }
 
-        // save và bảng customer
-        $cusumer = $cus->newEntity();
-        $cusumer->name = $req['full_name'];
-        $cusumer->email = $req['email'];
-        $cusumer->address = $req['address'];
-        $cusumer->phone_number = $req['phone'];
-        $cusumer->note = $req['notes'];
-        $cus->save($cusumer);
-        
-        // lấy giá trị cuối cùng (khácg hàng vừa mua cuối cùng )
-        $customerid = $cus->find()
-        ->select(['id', 'name'])
-        ->where(['id !=' => 1])
-        ->last();
+    
 
-        // save và bảng bill_details
-        $bill = $getbill->newEntity();
-        $bill->id_customer = $customerid->id;
-        $bill->date_order = date('Y-m-d');
-        $bill->total = $totalPrice;
-        $bill->payment = $req['payment_method'];
-        $bill->note = $req['notes'];
-        $getbill->save($bill);
-        
-        // lấy bill id trong bảng bills
-        $billid = $getbill->find()
-        ->select(['id'])
-        ->where(['id !=' => 1])
-        ->last();
+     public function customer($id)
+    {
+        $this->listbill($id);
 
-        foreach ($cart as $key) {
-         
-            $bill_detail = $getbill->newEntity();
-            $bill_detail->id_bill = $billid['id'];
-            $bill_detail->id_product = $key['id'];//$value['item']['id'];
-            $bill_detail->quantity = $key['quantity'];
-            $bill_detail->subtotal = $key['quantity'] *  $key['price'];
-            $bill_detail->unit_price = $key['price'];
-            $get_bill_detail->save($bill_detail);
-        }
-
-       
-                   
-        $array  = array('name'=>$name,'email'=>$email,'address'=>$address, 'phone'=>$phone ,'notes'=>$notes ,'payment_method'=>$payment_method);
-        $this->Email->sendUserEmail($email,'CakeFoody',$array,'order');
-                    
-                    
-        
-        $session->delete('cart');
-        $session->delete('payment.total');
-        $this->Flash->success(__('Cảm ơn bạn đã mua sản phẩm của chúng tôi. Đơn hàng đang được sử lý'));
-        $this->redirect(URL_INDEX);
-        
     }
 
 
@@ -236,17 +209,14 @@ class ProductsController  extends AppController{
         $this->set(compact('customers','typeproducts'));
     }
     
-    public function listbill($id)
+    public function billdetail($id)
     {
-
         $this->readtypeproduct();
-
         $customer = TableRegistry::get('customer');
         $bill = TableRegistry::get('bills');
         $bill_detail = TableRegistry::get('bill_detail');
 
         $getid = $customer->get($id)->id;
-
         $customers = $customer->find('all');
         $bills = $bill->find('all')
         ->where(['bills.id_customer =' => $getid])
@@ -256,7 +226,7 @@ class ProductsController  extends AppController{
 
         $listbills = $bill->find()
         ->select(['c.name','c.email','c.address','c.phone_number','c.note','bills.total',
-            'b.quantity','b.unit_price','p.name','p.image'
+            'b.quantity','b.unit_price','b.created_at','p.name','p.image'
         ])
         ->where(['c.id =' => $getid])
         ->hydrate(false)
@@ -288,18 +258,110 @@ class ProductsController  extends AppController{
     public function getSearch(){
          $this->readtypeproduct();
         $key= $this->request->query;
-        $find = $this->Products->find("all")
-       ->where(['name LIKE' => '%'.$key['key'].'%']);
 
+        $findname = $this->Products->find("all")
+       ->where(['name LIKE' => '%'.$key['key'].'%']);
         $this->paginate= array(
             'order' => [
             'find.id' => 'asc'
             ]
         );
-        $product = $this->paginate($find);
-        $this->set('products',$product);
+        $products = $this->paginate($findname);
+        if (count($products)== 0) {
+            $findid = $this->Products->find("all")
+               ->where(['id =' => $key['key'].'%']);
+                $this->paginate= array(
+                    'order' => [
+                    'find.id' => 'asc'
+                    ]
+                );
+               $products = $this->paginate($findid);
+        }
+        
+        $this->set(compact('products'));
 
      }
+
+     public function postCheckout(){
+        
+        $session = $this->request->session();
+        // khai báo database để lấy dữ liệu table
+        $cus = TableRegistry::get('customer');
+        $getbill = TableRegistry::get('bills');
+        $get_bill_detail = TableRegistry::get('bill_detail');
+        $get_email = TableRegistry::get('get_email');
+
+        // read session
+        $readuser = $session->read('Auth.User');
+        $cart = $session->read('cart');
+        $totalPrice = $session->read('payment.total');
+        $req= $this->request->data();
+        $readcustomer = $cus->find('all');
+
+        if($readuser['permission']==1 || $readuser['permission']== 2 ){
+            $cusumer->id_user = $readuser['id'];
+        }
+
+        $cusumer = $cus->newEntity();
+        $cusumer->name = $req['full_name'];
+        $cusumer->id_user = 0;
+        $cusumer->email = $req['email'];
+        $cusumer->address = $req['address'];
+        $cusumer->phone_number = $req['phone'];
+        $cusumer->note = $req['notes'];
+        $cus->save($cusumer);
+        
+        // lấy giá trị cuối cùng (khácg hàng vừa mua cuối cùng )
+        $customerid = $cus->find()
+        ->select(['id', 'name'])
+        ->where(['id !=' => 1])
+        ->last();
+
+        //save và bảng get_email
+        $get = $get_email->newEntity();
+        $get->email = $req['email'];
+        $get_email->save($get);
+
+        // save và bảng bill_details
+        $bill = $getbill->newEntity();
+        $bill->id_customer = $customerid->id;
+        $bill->date_order = date('Y-m-d');
+        $bill->total = $totalPrice;
+        $bill->payment = $req['payment_method'];
+        $bill->note = $req['notes'];
+        $getbill->save($bill);
+        
+        // lấy bill id trong bảng bills
+        $billid = $getbill->find()
+        ->select(['id'])
+        ->where(['id !=' => 1])
+        ->last();
+
+        foreach ($cart as $key) {
+         
+            $bill_detail = $getbill->newEntity();
+            $bill_detail->id_bill = $billid['id'];
+            $bill_detail->id_product = $key['id'];//$value['item']['id'];
+            $bill_detail->quantity = $key['quantity'];
+            $bill_detail->subtotal = $key['quantity'] *  $key['price'];
+            $bill_detail->unit_price = $key['price'];
+            $get_bill_detail->save($bill_detail);
+        }
+
+
+       
+                   
+        $array  = array('name'=>$cusumer->name);
+        $this->Email->sendUserEmail($cusumer->email,'CakeFoody',$array,'order');
+                    
+                    
+        
+        $session->delete('cart');
+        $session->delete('payment.total');
+        $this->Flash->success(__('Cảm ơn bạn đã mua sản phẩm của chúng tôi. Đơn hàng đang được sử lý'));
+        $this->redirect(URL_INDEX);
+        
+    }
 
      public function readtypeproduct()
      {
@@ -342,13 +404,13 @@ class ProductsController  extends AppController{
         
 
         $type = $this->Products->find("all")
-        ->where(['products.id_type >=' => $product->id_type ],
-                ['products.id !=' => $product->id]
+        ->where(['products.id_type >=' => $product->id_type ]
             );
         $this->paginate= array(
-            'limit' => 9,
+            'limit' => 10,
             'order' => [
-            'products.id' => 'asc'
+            'products.id' =>' asc',
+
             ]
         );
         $producttype = $this->paginate($type);
@@ -359,7 +421,6 @@ class ProductsController  extends AppController{
         ->where(['products.new >=' => 1 ]);
 
         $this->set('productnew',$new);
-
     }
 
 	public function addproduct()
@@ -374,17 +435,16 @@ class ProductsController  extends AppController{
             $dir = WWW_ROOT .'img\uploads\ '. $name['name'] ;
             $a = move_uploaded_file($data['image']['tmp_name'], $dir);
             $product['image'] = '/webroot/img/uploads/'.$name['name'];
+            if ($this->Products->save($product)) 
+            {       
+                $this->Flash->success(__('Sản Phẩm đã được thêm.'));
+                
+                return $this->redirect(URL_INDEX);
+            } else {
+                $this->Flash->error(__('Sản phẩm chưa được lưu. Thử lại.'));
+            }   
         }
         
-        // //now do the save
-        if ($this->Products->save($product)) 
-        {       
-            $this->Flash->success(__('The product has been saved.'));
-            
-            return $this->redirect(URL_INDEX);
-        } else {
-            $this->Flash->error(__('The product could not be saved. Please, try again.'));
-        }   
         $this->set('product',$product);
     }
     
@@ -395,12 +455,30 @@ class ProductsController  extends AppController{
 
     public function editproduct($id)
     {   
-        $this->readtypeproduct();
-        $product = $this->Products->get($id);
+       $this->readtypeproduct();
+       $product = $this->Products->get($id);
+       $get = $this->Products->find("all")
+        ->select(['c.name'])
+        ->hydrate(false)
+        ->join([
+            'table' => 'typeProducts',
+            'alias' => 'c',
+            'type' => 'LEFT',
+            'conditions' => array(
+                'c.id = products.id_type',
+                )     
+        ])
+        ->where(['products.id_type =' => $product->id_type ])
+        ->toArray();
+        // $getname = $get['0']['name'];
+        $getname = $get['0']['c']['name'];
+
+       
         $image = $product->image;
         if ($this->request->is(['post', 'put'])) {
             //$name = $this->request->data['name'];
             $data = $this->request->data();
+            
             $this->Products->patchEntity($product, $this->request->data);
             
                 $name=$data['image'];
@@ -414,24 +492,26 @@ class ProductsController  extends AppController{
                         pr( $product['image']);
                }
             
-
             if ($this->Products->save($product)) {
-                $this->Flash->success(__('Your information data has been updated.'));
+                $this->Flash->success(__('Thông tin của sản phẩm đã được cập nhật.'));
                 
                 return $this-> redirect(array('action' => 'editproduct', $product->id));
             }
-            $this->Flash->error(__('Unable to update your profile.'));
+            $this->Flash->error(__('Không thể cập nhập thông tin cho sản phẩm. Thử lại .'));
         }
-        $this->set('product', $product);        
+        $this->set(compact('product', 'getname'));        
     }
 
     public function delete($id)
     {
+
         $this->request->allowMethod(['post', 'delete']);
-    
+        
         $product = $this->Products->get($id);
-        if ($this->Products->delete($product)) {
-            $this->Flash->success(__('The user with id: {0} has been deleted.', h($id)));
+var_dump($id);
+die;
+        if ($this->Products->delete($id)) {
+            $this->Flash->success(__('Sản phẩm có id: {0} đã được xóa.', h($id)));
             return $this->redirect(URL_INDEX);
         }       
         
